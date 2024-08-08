3f9107a10b1dc1e1daa9c9b60f754fa089bf51cd
Alen Keneddy Aguilar Jara <alenaguilar24@gmail.com>
2024-08-08 17:51:06 -0500

    HelloWordl and PassWord Generator

---
+++ MyHelloWord/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("¿Cual es tu nombre?");
+var nombre= Console.ReadLine();
+var fechayhoraactual= DateTime.Now;
+
+
+Console.WriteLine($"{Environment.NewLine}Hola, {nombre}, hoy es: {fechayhoraactual}");
+Console.WriteLine($"{Environment.NewLine}Presiona Cualquier Techa para detener la ejecucion ");
+Console.ReadKey(true);
+
---
+++ MyPasswordGenerator/Program.cs
@@ -0,0 +1,5 @@
+﻿using PasswordGenerator;
+
+var pwd = new Password();
+var password = pwd.Next();
+Console.WriteLine($"Tu contraseña segura es {password}");
